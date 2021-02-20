8d7a3cbc3dad1bf260fada45c769877f9e41bffb
MrWillCom <mr.will.com@outlook.com>
2021-02-20 03:47:08 +0000

    Update animation curve for win32 and Add stylesheets for darwin

--- src/ts/windows/components/dropdowns.ts
+++ src/ts/windows/components/dropdowns.ts
@@ -14,7 +14,8 @@
 
         e["__proto__"].isOpen = false;
 
-        e["__proto__"].open = () => {
+        e["__proto__"].open = (ev: MouseEvent) => {
+            (e as HTMLElement).style.setProperty("--clicked-item-height", `${(ev.target as HTMLElement).clientHeight + 4}px`);
             e.classList.add("open");
             e["__proto__"].isOpen = true;
         }
@@ -24,11 +25,11 @@
             e["__proto__"].isOpen = false;
         }
 
-        e["__proto__"].toggle = () => {
+        e["__proto__"].toggle = (ev: MouseEvent) => {
             if (e["__proto__"].isOpen) {
                 e["__proto__"].close();
             } else {
-                e["__proto__"].open();
+                e["__proto__"].open(ev);
             }
         }
     }
